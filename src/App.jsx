369dfe82560fb91bc22eef2ba45b445714e52c26
Hirak Mahata <hmahata591@gmail.com>
2024-05-30 21:56:43 +0530

    completed welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,123 +1,42 @@
-// App.js
-import { useState } from "react";
-import {
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Avatar,
-  Box,
-} from "@mui/material";
-import BorderColorIcon from "@mui/icons-material/BorderColor";
-import {
-  Toolbar,
-  IconButton,
-  Drawer,
-  List,
-  CssBaseline,
-  // Typography,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import { useTheme } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import system from "./assets/system.svg";
-import ChatComponent from "./components/Chat";
+import "./App.css";
+import systemLogo from "./assets/system.svg";
+import edit from "./assets/edit.svg";
+import Welcome from "./components/welcome/Welcome";
+import { IoSendSharp } from "react-icons/io5";
+import { IoIosSave } from "react-icons/io";
 
-const drawerWidth = 240;
-
-function ResponsiveDrawer() {
-  const [mobileOpen, setMobileOpen] = useState(false);
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
-  const drawer = (
-    <div>
-      <List>
-        <ListItem button key="New Chat">
-          <Box sx={{ display: "flex", alignItems: "center", width: "100%" }}>
-            <ListItemIcon>
-              <Avatar src={system} />
-            </ListItemIcon>
-            <ListItemText primary="New Chat" sx={{ flexGrow: 1 }} />
-            <BorderColorIcon />
-          </Box>
-        </ListItem>
-        <ListItem button key="Past Conversations">
-          <ListItemText primary="Past Conversations" />
-        </ListItem>
-      </List>
+const App = () => {
+  return (
+    <div className="app-container">
+      <div className="sidebar">
+        <div className="new-chat">
+          <img src={systemLogo} alt="sidebar-logo" className="sidebar-logo" />
+          <h2>New Chat</h2>
+          <img src={edit} alt="new-chat-edit" />
+        </div>
+        <div className="past-conversations">
+          <h3>Past Conversations</h3>
+        </div>
+      </div>
+      <div className="main-container">
+        <div className="app-heading">
+          <h1>Bot AI</h1>
+        </div>
+        <Welcome />
+        <div className="app-inputs">
+          <input placeholder="Message Bot AI" />
+          <div className="ask">
+            <button>Ask</button>
+            <IoSendSharp size={20} />
+          </div>
+          <div className="save">
+            <button>Save</button>
+            <IoIosSave size={20} />
+          </div>
+        </div>
+      </div>
     </div>
   );
+};
 
-  return (
-    <Box sx={{ display: "flex", alignItems: "flex-start" }}>
-      <CssBaseline />
-      <Toolbar>
-        {isMobile && (
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-        )}
-      </Toolbar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          variant={isMobile ? "temporary" : "permanent"}
-          open={isMobile ? mobileOpen : true}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: "none", sm: "block" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          height: "100vh",
-        }}
-      >
-        <ChatComponent />
-      </Box>
-    </Box>
-  );
-}
-
-export default ResponsiveDrawer;
+export default App;
---
+++ src/components/welcome/Welcome.jsx
@@ -0,0 +1,33 @@
+import "./Welcome.css";
+import systemLogo from "../../assets/system.svg";
+
+const Welcome = () => {
+  return (
+    <div className="welcome-container">
+      <div className="welcome-heading">
+        <h1>How Can I Help You Today?</h1>
+        <img src={systemLogo} alt="welcome-logo" />
+      </div>
+      <div className="welcome-grid">
+        <div>
+          <h3>Hi, what is the weather</h3>
+          <p>Get immediate AI generated response</p>
+        </div>
+        <div>
+          <h3>Hi, what is the weather</h3>
+          <p>Get immediate AI generated response</p>
+        </div>
+        <div>
+          <h3>Hi, what is the weather</h3>
+          <p>Get immediate AI generated response</p>
+        </div>
+        <div>
+          <h3>Hi, what is the weather</h3>
+          <p>Get immediate AI generated response</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
